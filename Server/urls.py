88a59ceb6668dd0348b912c64b3727fe6fb98bd5
daodat2000 <daodat2000@gmail.com>
2021-01-14 14:12:03 +0700

    14/1

--- Server/urls.py
+++ Server/urls.py
@@ -21,6 +21,15 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^login', views.Login, name='login'),
+    #http://192.168.1.3:8080/login?u=fit18&p=fit18
+    url(r'^signup', views.Signup, name='signup'),
+    #http://192.168.1.3:8080/signup?u=nguyenhoangquan&p=nguyenhoangquan
     url(r'^trip', views.getTripUser, name="trip"),
-    url(r'^expense', views.getExpenseTrip, name="expense")
+    #http://192.168.1.3:8080/trip?id=1
+    url(r'^addtrip', views.addTrip, name="addtrip"),
+    #http://192.168.1.3:8080/addtrip?idu=3&name=a&bud=2000000&start=12&end=13&dep=HCM
+    url(r'^expense', views.getExpenseTrip, name="expense"),
+    #http://192.168.1.3:8080/expense?t=1
+    url(r'^addexpense', views.addExpense, name="addexpense")
+    #http://192.168.1.3:8080/addexpense?idt=1&name=an&cost=200000&type=2
 ]
--- Tripplaner/views.py
+++ Tripplaner/views.py
@@ -5,21 +5,42 @@ from django.http import FileResponse
 from .models import *
 # Create your views here.
 def Login(request):
-    name = request.GET['u']
+    uname = request.GET['u']
     passw = request.GET['p']
         
     ac = Account.objects.all()
     for acci in ac:
         truename = acci.getUserName()
         truepass = acci.getPassword()
-        if name == truename and passw == truepass:
-            obj = {"idUser": acci.getIDUser()}
+        if uname == truename and passw == truepass:
+            obj = {"idUser": acci.getIDUser(), "userName": acci.getUserName(), "name": acci.getName(), "dateOfBirth": acci.getDateOfBirth(), "placeOfBirth": acci.getPlaceOfBirth()}
             return JsonResponse(obj)
             break
         else:
             continue
     return HttpResponse('Login fail')
     
+    pass
+
+def Signup(request):
+    uname = request.GET['u']
+    passw = request.GET['p']
+    ID = 0    
+    ac = Account.objects.all()
+
+    for acci in ac:
+        truename = acci.getUserName()
+        ID = max(int(ID),int(acci.getIDUser()))
+        if uname == truename :
+            obj = {"idUser": ""}
+            return JsonResponse(obj)
+            break
+        else:
+            continue
+    ID = str(int(ID) + 1)
+    Account.objects.create(idUser = ID, userName = uname, password = passw)
+    obj = {"idUser": ID}
+    return JsonResponse(obj)
     pass
 
 def getTripUser(request):
@@ -35,6 +56,26 @@ def getTripUser(request):
     return JsonResponse(res)
     pass
 
+def addTrip(request):
+    idu = request.GET['idu']
+    name = request.GET['name']
+    bud = request.GET['bud']
+    start = request.GET['start']
+    end = request.GET['end']
+    dep = request.GET['dep']
+
+    ID = 0    
+    trip = Trip.objects.all()
+    for tripi in trip:
+        ID = max(int(ID),int(tripi.getIDTrip())) 
+    idUser = Account.objects.get(idUser=idu)
+    ID = str(int(ID) + 1)
+    Trip.objects.create(idTrip = ID, idUser = idUser, tripName=name, budget=bud, startDate=start, endDate=end, departure=dep)
+    obj = {"idTrip": ID}
+    return JsonResponse(obj)
+    pass
+
+
 def getExpenseTrip(request):
     id = request.GET['t']
     result = []
@@ -46,4 +87,21 @@ def getExpenseTrip(request):
             result.append(obj)
     res = {"result": result}
     return JsonResponse(res)
+    pass
+
+def addExpense(request):
+    idt = request.GET['idt']
+    name = request.GET['name']
+    cost = request.GET['cost']
+    typ = request.GET['type']
+
+    ID = 0    
+    ex = Expense.objects.all()
+    for exi in ex:
+        ID = max(int(ID),int(exi.getIDExpense())) 
+    idTrip = Trip.objects.get(idTrip=idt)
+    ID = str(int(ID) + 1)
+    Expense.objects.create(idExpense= ID, idTrip = idTrip, expenseName=name, cost=cost, typeExpense=typ)
+    obj = {"idExpense": ID}
+    return JsonResponse(obj)
     pass
