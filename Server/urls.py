f2f38539caa32730d2aa472c39c0afc6796b7a34
daodat2000 <daodat2000@gmail.com>
2021-01-15 00:18:47 +0700

    0:18

--- Server/urls.py
+++ Server/urls.py
@@ -20,28 +20,70 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^login', views.Login, name='login'),
+    path('updateuser', views.updateUser),
+    #http://192.168.1.3:8080/updateuser
+    # {
+    # "id": "1",
+    # "name": "abc",
+    # "dateOfBirth": "12",
+    # "placeOfBirth": "23"
+    # }
+    path('signup', views.Signup),
+    #http://192.168.1.3:8080/signup
+    # {
+    # "username": "1",
+    # "password": "1",
+    # "name": "12"
+    # }
+    path('updatetrip', views.updateTrip),
+    #http://192.168.1.3:8080/updatetrip
+    # {
+    # "id": "1", 
+    # "tripName": "a", 
+    # "budget": 2000000.0, 
+    # "startDate": "12", 
+    # "endDate": "13", 
+    # "departure": "HCM1"
+    # }
+    path('addtrip', views.addTrip),
+    #http://192.168.1.3:8080/addtrip
+    # {
+    # "idUser": "1", 
+    # "tripName": "adasd", 
+    # "budget": 2000000.0, 
+    # "startDate": "12", 
+    # "endDate": "13", 
+    # "departure": "HCM1"
+    # }
+    path('updateexpense', views.updateExpense),
+    #http://192.168.1.3:8080/updateexpense
+    # {
+    # "id": "1", 
+    # "expenseName": "an uong", 
+    # "cost": 200000.0, 
+    # "type": 3
+    # }
+
+    path('addexpense', views.addExpense),
+    #http://192.168.1.3:8080/addexpense
+    # {
+    # "idTrip": "1", 
+    # "expenseName": "an choi", 
+    # "cost": 300000.0, 
+    # "type": 3
+    # }
+
+    url(r'^login', views.Login),
     #http://192.168.1.3:8080/login?u=fit18&p=fit18
-    url(r'^getuserbyid', views.getUserByID, name='getuserbyid'),
+    url(r'^getuserbyid', views.getUserByID),
     #http://192.168.1.3:8080/getuserbyid?id=1
-    url(r'^changeuser', views.changeUser, name='changeuserbyid'),
-    #http://192.168.1.3:8080/changeuser?id=1&name=abc&date=12&place=HN
-    url(r'^signup', views.Signup, name='signup'),
-    #http://192.168.1.3:8080/signup?u=nguyenhoangquan&p=nguyenhoangquan
-    url(r'^trip', views.getTrip, name="trip"),
+    url(r'^trip', views.getTrip),
     #http://192.168.1.3:8080/trip?id=1
-    url(r'^gettripbyID', views.getTripByID, name="tripbyid"),
+    url(r'^gettripbyID', views.getTripByID),
     #http://192.168.1.3:8080/gettripbyID?id=1
-    url(r'^changetrip', views.changeTrip, name="changetripbyid"),
-    #http://192.168.1.3:8080/changetrip?id=1&name=a&bud=2000000&start=12&end=13&dep=HCM
-    url(r'^addtrip', views.addTrip, name="addtrip"),
-    #http://192.168.1.3:8080/addtrip?idu=3&name=a&bud=2000000&start=12&end=13&dep=HCM
-    url(r'^expense', views.getExpenseTrip, name="expense"),
+    url(r'^expense', views.getExpenseTrip),
     #http://192.168.1.3:8080/expense?id=1
-    url(r'^getexpensebyID', views.getExpensebyID, name="expensebyid"),
+    url(r'^getexpensebyID', views.getExpensebyID)
     #http://192.168.1.3:8080/getexpensebyID?id=1
-    url(r'^changexpense', views.changExpense , name="changexpense"),
-    #http://192.168.1.3:8080/changexpense?id=1&name=an&cost=200000&type=2
-    url(r'^addexpense', views.addExpense, name="addexpense")
-    #http://192.168.1.3:8080/addexpense?idt=1&name=an&cost=200000&type=2
+    
 ]
--- Tripplaner/views.py
+++ Tripplaner/views.py
@@ -7,6 +7,8 @@ from django.http import FileResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
 # Create your views here.
+
+
 def Login(request):
     uname = request.GET['u']
     passw = request.GET['p']
@@ -40,20 +42,27 @@ def getUserByID(request):
     pass
 
 @csrf_exempt
-def changeUser(request):
-    id = request.GET['id']
-    name = request.GET['name']
-    date = request.GET['date']
-    place = request.GET['place']
-    Account.objects.filter(idUser=id).update(name=name,dateOfBirth=date, placeOfBirth=place)
+def updateUser(request):
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+
+    id = body['id']
+    name = body['name']
+    dateOfBirth = body['dateOfBirth']
+    placeOfBirth = body['placeOfBirth']
+    Account.objects.filter(idUser=id).update(name=name,dateOfBirth=dateOfBirth, placeOfBirth=placeOfBirth)
     obj = {"result": 1}
     return JsonResponse(obj)
     pass
 
 @csrf_exempt
 def Signup(request):
-    uname = request.GET['u']
-    passw = request.GET['p']
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+
+    uname = body['username']
+    passw = body['password']
+    name = body['name']
     ID = 0    
     ac = Account.objects.all()
 
@@ -67,7 +76,7 @@ def Signup(request):
         else:
             continue
     ID = str(int(ID) + 1)
-    Account.objects.create(idUser = ID, userName = uname, password = passw)
+    Account.objects.create(idUser = ID, userName = uname, password = passw,name=name)
     obj = {"result": 1}
     return JsonResponse(obj)
     pass
@@ -91,33 +100,40 @@ def getTripByID(request):
     trip = Trip.objects.all()
     for tripi in trip:
         idTrip =  tripi.getIDTrip()
-        if id == idTrip.idTrip:
+        if id == idTrip:
             obj = {"id": tripi.getIDTrip(), "tripName": tripi.getTripName(), "budget": tripi.getBudget(), "startDate": tripi.getStartDate(), "endDate": tripi.getEndDate(), "departure": tripi.getDeparture()}
             result.append(obj)
     res = {"result": result}
     return JsonResponse(res)
     pass
 
-def changeTrip(request):
-    id = request.GET['id']
-    name = request.GET['name']
-    bud = request.GET['bud']
-    start = request.GET['start']
-    end = request.GET['end']
-    dep = request.GET['dep']
+@csrf_exempt
+def updateTrip(request):
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+
+    id = body['id']
+    name = body['tripName']
+    bud = body['budget']
+    start = body['startDate']
+    end = body['endDate']
+    dep = body['departure']
    
     Trip.objects.filter(idTrip=id).update(tripName=name, budget=bud, startDate=start,endDate=end,departure=dep)
     obj = {"result": 1}
     return JsonResponse(obj)
 
+@csrf_exempt
 def addTrip(request):
-    idu = request.GET['idu']
-    name = request.GET['name']
-    bud = request.GET['bud']
-    start = request.GET['start']
-    end = request.GET['end']
-    dep = request.GET['dep']
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
 
+    idu = body['idUser']
+    name = body['tripName']
+    bud = body['budget']
+    start = body['startDate']
+    end = body['endDate']
+    dep = body['departure']
     ID = 0    
     trip = Trip.objects.all()
     for tripi in trip:
@@ -149,27 +165,35 @@ def getExpensebyID(request):
     expense = Expense.objects.all()
     for exi in expense:
         idExpense = exi.getIDExpense()
-        if id == idExpense.idExpense:
+        if id == idExpense:
             obj = {"id": exi.getIDExpense(), "expenseName": exi.getExpenseName(), "cost": exi.getCost(), "type": exi.getTypeExpense()}        
             result.append(obj)
     res = {"result": result}
     return JsonResponse(res)
     pass
 
-def changExpense(request):
-    id = request.GET['id']
-    name = request.GET['name']
-    cost = request.GET['cost']
-    typ = request.GET['type']
+@csrf_exempt
+def updateExpense(request):
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+
+    id = body['id']
+    name = body['expenseName']
+    cost = body['cost']
+    typ = body['type']
     Expense.objects.filter(idTrip=id).update(expenseName=name, cost=cost, typeExpense=typ)
     obj = {"result": 1}
     return JsonResponse(obj)
 
+@csrf_exempt
 def addExpense(request):
-    idt = request.GET['idt']
-    name = request.GET['name']
-    cost = request.GET['cost']
-    typ = request.GET['type']
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+
+    idt = body['idTrip']
+    name = body['expenseName']
+    cost = body['cost']
+    typ = body['type']
 
     ID = 0    
     ex = Expense.objects.all()
